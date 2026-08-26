f55ea87a02dfaf73e9e5552a425dd36e9b4e431e
claymicholaz <claymicholaz@gmail.com>
2026-08-26 23:20:05 +0700

    fix: add event page

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,15 +1,15 @@
-// import Layout from "@/components/Layout";
-// import React from "react";
+import Layout from "@/components/Layout";
+import React from "react";
 
-// export default function AcaraPage() {
-//   return (
-//     <Layout
-//       title="Acara - BIOS"
-//       description="Informasi mengenai acara-acara yang diselenggarakan oleh HIMA BIOS Universitas Bunda Mulia"
-//     >
-//       <div>
+export default function AcaraPage() {
+  return (
+    <Layout
+      title="Acara - BIOS"
+      description="Informasi mengenai acara-acara yang diselenggarakan oleh HIMA BIOS Universitas Bunda Mulia"
+    >
+      <div>
         
-//       </div>
-//     </Layout>
-//   );
-// }
+      </div>
+    </Layout>
+  );
+}
